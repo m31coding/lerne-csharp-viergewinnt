c8cdd7cc1893b8fd3462b48babefb0ee718e6ac8
Kevin <Kevin.Schaal@Convexis.de>
2021-02-05 15:20:57 +0100

    Cleanup

--- VierGewinnt/Spieler/Heuristiken/LooserHeuristik.cs
+++ VierGewinnt/Spieler/Heuristiken/LooserHeuristik.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VierGewinnt.Spiel;
+﻿using VierGewinnt.Spiel;
 
 namespace VierGewinnt.Spieler.Heuristiken
 {
